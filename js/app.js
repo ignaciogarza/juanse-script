13d274b107923806d51cb7cc142c267b106ea5f9
Ignacio Garza <ignacioagarza@gmail.com>
2020-09-05 10:18:04 -0500

    list countries in the file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const extraer = document.getElementById("iniciarExtraccion");
 const file = document.querySelector("input[type='file']");
+const menuCountries = document.getElementById("itemCountries");
+
 let listCountries = [];
 
 extraer.addEventListener("click", function (e) {
@@ -8,24 +10,51 @@ extraer.addEventListener("click", function (e) {
     const lines = reader.result.split("\n").map(function (line) {
       return line.split(",");
     });
-    createPerson(lines);
+
+    getInformationFromLines(lines);
   };
-
-  console.log(listCountries);
-
   reader.readAsText(file.files[0]);
 });
 
-function createPerson(persons) {
-  console.log(persons);
-  for (person in persons) {
-    createCountries(persons[person][1]);
+function getInformationFromLines(lines) {
+  // crate array of countries from countries in file
+  for (line in lines) {
+    if (lines[line][0] === "ACE Edition" || lines[line][0] === "") {
+      console.log("Saltarte el titulo / vacio");
+    } else {
+      crateCountryList(lines[line][1]);
+    }
   }
 
-  //   createCountries(person);
+  //Draw HTML menu of countries
+  drawMenuCountries(listCountries);
 }
 
-function createCountries(country) {
-  console.log("hola pais");
-  console.log(country);
+// create contries list
+
+function crateCountryList(country) {
+  if (!listCountries.includes(country)) {
+    if (country !== null) {
+      listCountries.push(country);
+    }
+  }
+}
+
+// Draw HTML Menu structure for menu countries
+
+function drawMenuCountries(countries) {
+  console.log(countries);
+  for (countrie in countries) {
+    menuCountries.innerHTML += `<li><a href="#${countries[countrie]}">${countries[countrie]}</a></li>`;
+  }
+}
+
+// draw person profile by country
+
+function createPerson(persons) {
+  let countries = [];
+  console.log(persons);
+  for (person in persons) {
+    countries.push(createCountries(persons[person][1]));
+  }
 }
