f23703d9cffe428e676f13f685b4fc9bb90a66c1
Ignacio Garza <ignacioagarza@gmail.com>
2020-09-06 11:56:39 -0500

    fixed on data split by commas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const extraer = document.getElementById("iniciarExtraccion");
 const file = document.querySelector("input[type='file']");
 const menuCountries = document.getElementById("itemCountries");
-const frameContent = document.getElementById("contentEvent");
+const countryTitle = document.getElementById("countryTitle");
 
 // flags
 const flagAntigua =
@@ -26,12 +26,13 @@ const flagCostaRica =
   "http://riacevents.org/ACE/wp-content/uploads/2020/09/Costa-Rica.png";
 
 let listCountries = [];
+const attende = [];
 
 extraer.addEventListener("click", function (e) {
   const reader = new FileReader();
   reader.onload = function () {
     const lines = reader.result.split("\n").map(function (line) {
-      return line.split(",");
+      return line;
     });
 
     getInformationFromLines(lines);
@@ -41,18 +42,23 @@ extraer.addEventListener("click", function (e) {
 
 function getInformationFromLines(lines) {
   // crate array of countries from countries in file
+
   for (line in lines) {
-    if (lines[line][0] === "ACE Edition" || lines[line][0] === "") {
+    attende.push(lines[line].split(","));
+  }
+
+  for (person in attende) {
+    if (attende[person][0] === "ACE Edition" || attende[person][0] === "") {
       console.log("Saltarte el titulo / vacio");
     } else {
-      crateCountryList(lines[line][1]);
+      crateCountryList(attende[person][1]);
     }
   }
   //Draw HTML menu of countries
   drawMenuCountries(listCountries);
 
   // Draw content about events
-  createCountryWithPersonProfile(lines);
+  createCountryWithPersonProfile(listCountries, attende);
 }
 
 // create contries list
@@ -68,7 +74,6 @@ function crateCountryList(country) {
 // Draw HTML Menu structure for menu countries
 
 function drawMenuCountries(countries) {
-  console.log(countries);
   for (countrie in countries) {
     menuCountries.innerHTML += `<li><a href="#${countries[countrie]}">${countries[countrie]}</a></li>`;
   }
@@ -76,10 +81,74 @@ function drawMenuCountries(countries) {
 
 // draw person profile by country
 
-function createCountryWithPersonProfile(persons) {
+function createCountryWithPersonProfile(countries, persons) {
+  for (country in countries) {
+    let flag;
+
+    if (countries[country] === "Argentina") {
+      flag = flagArgentina;
+    } else if (countries[country] === "Antigua & Barbuda") {
+      flag = flagAntigua;
+    } else if (countries[country] === "Barbados") {
+      flag = flagBarbados;
+    } else if (countries[country] === "Belize") {
+      flag = flagBelize;
+    } else if (countries[country] === "Bolivia") {
+      flag = flagBolivia;
+    } else if (countries[country] === "Brazil") {
+      flag = flagBrasil;
+    } else if (countries[country] === "Canada") {
+      flag = flagCanada;
+    } else if (countries[country] === "Chile") {
+      flag = flagChile;
+    } else if (countries[country] === "Colombia") {
+      flag = flagColombia;
+    } else if (countries[country] === "Costa Rica") {
+      flag = flagCostaRica;
+    }
+
+    countryTitle.innerHTML += `
+    <div class="infoPais" id="${countries[country]}" style=" vertical-align: top; padding-bottom: 16px; margin-bottom:40px;" id="contentEvent"><div class="banderaPais" style="display: inline-block"><img src="${flag}" alt="" style="width: 60px; height: 61px"/></div><div class="pais" style="display: inline-block; vertical-align: top; width:90%;" ><h2 style=" font-size: 40px; color: #406181; font-weight: 700; margin: 5px 0 0 20px; padding: 0;"  >${countries[country]}</h2></div></div>`;
+  }
+
+  let liPersonItem;
+
   for (person in persons) {
-    if (persons[person][1] === "Argentina") {
-      frameContent.innerHTML = "<h1>list</h1>";
+    if (persons[person][0] === "ACE Edition" || persons[person][0] === "") {
+    } else {
+      let name = persons[person][2].replace('"""', "");
+      let currentTitle = persons[person][3].replace('"""', "");
+      let participatedAs = persons[person][4].replace('"""', "");
+      let edition = persons[person][0].replace('"""', "");
+      edition = edition.replace('"""', "");
+
+      liPersonItem = document.createElement("li");
+      liPersonItem.style.cssText =
+        "display:block; border-bottom:2px solid black; padding-bottom:20px; margin-bottom:20px;";
+      liPersonItem.innerHTML = `
+      <h3 style=" color: #cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">Name</h3>
+      <p style="font-size: 20px; color: #53969e; font-weight: 500; margin-top: 0; padding-top: 0;">${name}</p>
+      <h3 style="color:#cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">Current title</h3>
+      <p style="font-size: 20px; color: #53969e;font-weight: 500; margin-top: 0; padding-top: 0;">${currentTitle}</p> 
+      `;
+      if (participatedAs) {
+        liPersonItem.innerHTML += `<h3 style="color: #cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">* Participated as</h3>
+      <p style="font-size: 20px; color: #53969e; font-weight: 500; margin-top: 0; padding-top: 0;">${participatedAs}</p>`;
+      }
+
+      liPersonItem.innerHTML += `
+      <h3 style="color: #cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">ACE Edition</h3>
+      <p style=" font-size: 20px; color: #53969e; font-weight: 500; margin-top: 0; padding-top: 0;">${edition}</p>
+      <h3 style=" color: #cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">Social Media</h3>
+      <p style=" font-size: 20px; color: #53969e; font-weight: 500; margin-top: 0; padding-top: 0;"><i class="fab fa-twitter"></i><i class="fab fa-twitter"></i>xxxx</p>
+      <a href="${persons[person][6]}" target="_blank" style="text-decoration: none; color: #cdcdcd">
+      <h3 style="color: #cdcdcd; font-weight: 400; font-size: 26px; margin-bottom: 0;">
+      <i aria-hidden="true" class="fas fa-link"></i> Website
+      </h3></a>`;
+
+      document
+        .getElementById(`${persons[person][1]}`)
+        .appendChild(liPersonItem);
     }
   }
 }
